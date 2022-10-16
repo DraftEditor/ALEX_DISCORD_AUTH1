50611fce0c3203375d774d7010e55ee8981e8b14
Draft <45689230+DraftEditor@users.noreply.github.com>
2022-10-16 18:34:06 +0000

    ec

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 import subprocess
-subprocess.run("python BOT.py & python Flask.py", shell=True, stdout=subprocess.PIPE)
+
+proc = subprocess.run("python BOT.py & python Flask.py",
+                      shell=True,
+                      stdout=subprocess.PIPE)
 outs, errs = proc.communicate()
 print(outs.decode("ascii"))
